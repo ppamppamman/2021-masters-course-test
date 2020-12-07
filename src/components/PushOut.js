4023fb2d1c1d1c43766f5f4b0d883ae9cc72a9ee
Jaeuk-YOO <ryou9305@naver.com>
2020-12-07 11:01:37 +0900

    Add: Init function과 Push 기능의 구현
    async await 처리를 진행

--- src/components/PushOut.js
+++ src/components/PushOut.js
@@ -12,3 +12,15 @@ function input() {
 		});
 	});
 }
+function push(word, count, direction) {
+	return word;
+}
+
+async function init() {
+	let inputValueArr = await input();
+	inputValueArr = await inputValueArr.split(' ');
+	console.log('input is ', inputValueArr);
+	console.log(push(inputValueArr[0], inputValueArr[1], inputValueArr[2]));
+}
+
+init();
