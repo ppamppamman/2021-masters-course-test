33724dd242ae980eaa26000ed67ac7959479a8d5
Jaeuk-YOO <ryou9305@naver.com>
2020-12-07 11:00:48 +0900

    Add: Readline을 활용한 Input function 구현
    Promise를 통해 비동기적으로 값을 처리하도록 함

---
+++ src/components/PushOut.js
@@ -0,0 +1,14 @@
+function input() {
+	const readline = require('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	return new Promise((resolve) => {
+		rl.question('> ', (answer) => {
+			rl.close();
+			resolve(answer);
+		});
+	});
+}
