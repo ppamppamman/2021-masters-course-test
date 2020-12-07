983287e9477cda4c9d2e83cdec6d1d54b2fc2942
Jaeuk-YOO <ryou9305@naver.com>
2020-12-07 11:28:14 +0900

    Fix: Push 함수 인자값을 직접 변경하지 않도록 수정
    - input object를 활용
    - correction 함수로 direction, count를 핸들링하도록 함

--- src/components/PushOut.js
+++ src/components/PushOut.js
@@ -12,27 +12,54 @@ function input() {
 		});
 	});
 }
-function push(word, count, direction) {
-	while (count != 0) {
-		if (direction == 'L') {
-			let target = word[0];
-			word = word.substring(1, word.length);
-			word = word + target;
-		} else {
-			let target = word[word.length - 1];
-			word = word.substring(0, word.length - 1);
-			word = target + word;
-		}
-		count -= 1;
+function correction(count, direction) {
+	count = Math.abs(count);
+	if (direction == 'L' || direction == 'l') {
+		direction = 'R';
+	} else if (direction == 'R' || direction == 'r') {
+		direction = 'L';
 	}
-	return word;
+	return [count, direction];
+}
+
+function push(word, count, direction) {
+	let input = {};
+	[input.word, input.count, input.direction] = [word, count, direction];
+
+	if (count < 0) {
+		[input.count, input.direction] = correction(count, direction);
+	}
+
+	while (input.count != 0) {
+		let target;
+		if (input.direction == 'L' || input.direction == 'l') {
+			target = input.word[0];
+			input.word = input.word.substring(1, input.word.length);
+			input.word = input.word + target;
+		} else if (input.direction == 'R' || input.direction == 'r') {
+			target = input.word[input.word.length - 1];
+			input.word = input.word.substring(0, input.word.length - 1);
+			input.word = target + input.word;
+		}
+		input.count -= 1;
+	}
+	return input.word;
 }
 
 async function init() {
-	let inputValueArr = await input();
-	inputValueArr = await inputValueArr.split(' ');
-	console.log('input is ', inputValueArr);
-	console.log(push(inputValueArr[0], inputValueArr[1], inputValueArr[2]));
+	console.log('(To exit, press ^C again or ^D or type .exit)');
+	while (true) {
+		let values = await input();
+		if (values == '.exit') {
+			break;
+		} else if (values.split(' ').length != 3) {
+			console.log('wrong input');
+			continue;
+		}
+		values = values.split(' ');
+		let result = push(values[0], values[1], values[2]);
+		console.log(result);
+	}
 }
 
 init();
