438170c94b84d40f9368439e4a973f3fd5c58e81
Jaeuk-YOO <ryou9305@naver.com>
2020-12-12 06:01:58 +0900

    Add: Plane class 함수 설계 추가
    - getData, setData로 큐브면의 데이터를 불러오게끔 처리
    - rotateForward, rotateBackward로 시계방향 기준 정방향 역방향 처리
    - randomizeInit으로 랜덤처리까지 가능하게

--- src/components/step-3/Plane.js
+++ src/components/step-3/Plane.js
@@ -11,3 +11,13 @@ Plane.prototype.init = function (color) {
 	}
 	return rows;
 };
+
+Plane.prototype.randomizeInit = function () {}; // 향후 랜덤 처리를 위한 함수
+Plane.prototype.getData = function () {}; // 데이터 받아오기
+Plane.prototype.setData = function () {}; // 데이터 불러오기
+
+// 회전 정방향
+Plane.prototype.rotateForward = function () {};
+
+// 회전 역방향
+Plane.prototype.rotateBackward = function () {};
