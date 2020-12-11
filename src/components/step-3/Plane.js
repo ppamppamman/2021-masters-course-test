940a19eff0f4b9d97c537ac15b1ca0da7204bbff
Jaeuk-YOO <ryou9305@naver.com>
2020-12-12 06:16:59 +0900

    Add: Plane class getData, setData 구현, 주석변경

--- src/components/step-3/Plane.js
+++ src/components/step-3/Plane.js
@@ -13,8 +13,26 @@ Plane.prototype.init = function (color) {
 };
 
 Plane.prototype.randomizeInit = function () {}; // 향후 랜덤 처리를 위한 함수
-Plane.prototype.getData = function () {}; // 데이터 받아오기
-Plane.prototype.setData = function () {}; // 데이터 불러오기
+
+// 데이터 받아오기
+Plane.prototype.getData = function (indexes) {
+	let line = [];
+	for (const indexSet of indexes) {
+		let data = this.data[indexSet[0]][indexSet[1]];
+		line.push(data);
+	}
+	return line;
+};
+
+// 데이터 불러오기
+Plane.prototype.setData = function (indexes, newLine) {
+	if (newLine.length === 0) {
+		return;
+	}
+	for (const [i, indexSet] of indexes.entries()) {
+		this.data[indexSet[0]][indexSet[1]] = newLine[i];
+	}
+};
 
 // 회전 정방향
 Plane.prototype.rotateForward = function () {};
