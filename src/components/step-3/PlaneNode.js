f563a5abf07f043f7d7e713c23fe64c5cb61dc34
Jaeuk-YOO <ryou9305@naver.com>
2020-12-12 06:13:46 +0900

    Fix: 필요없는 주석 제거

--- src/components/step-3/PlaneNode.js
+++ src/components/step-3/PlaneNode.js
@@ -4,7 +4,6 @@ import PlaneEdge from './PlaneEdge.js';
 class PlaneNode {
 	constructor(planeSide, color) {
 		this.plane = new Plane(color);
-		// 면을 받아서 연결된 4방향의 라인을 받을 수 있게 추가 처리가 필요
 		this.edges = new PlaneEdge().init(planeSide);
 	}
 }
